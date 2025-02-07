695d02dbacd36e8c0b68584fb067829f3863ebb4
art3mis <diana@bendziula.com>
2025-02-07 13:12:41 +0100

    Add home/.config/fish/config.fish

---
+++ home/dot_config/private_fish/config.fish
@@ -0,0 +1,32 @@
+# vi: ft=config
+
+# init homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if status is-interactive
+    atuin init fish | source
+end
+
+# init mise
+mise activate fish | source
+
+# set config path
+set -gx XDG_CONFIG_HOME ~/.config
+
+# init gpg-agent
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
+# set default editor
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+# IntelliJ IDEA
+set PATH /Applications/IntelliJ\ IDEA.app/Contents/MacOS $PATH
+
+# fzf
+fzf_configure_bindings --directory=\cF
+
+# navi
+#navi widget fish | source
